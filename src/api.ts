eb8fefc8435f1a025d6fab1c066d1accdda97abf
TheAihopGG <108916224+TheAihopGG@users.noreply.github.com>
2025-01-04 11:05:48 +0700

    feat: open database, settings, graceful stop

---
+++ src/api.ts
@@ -0,0 +1,24 @@
+import * as dotenv from 'dotenv';
+import { loadSettings } from './services/settings';
+import * as sqlite3 from 'sqlite3';
+
+// init environments parameters
+dotenv.config();
+// load settings
+const settings = loadSettings(process.env.APP_SETTINGS_PATH);
+// open database
+const db = new sqlite3.Database(
+    settings.paths.database,
+    sqlite3.OPEN_CREATE | sqlite3.OPEN_READWRITE
+);
+console.debug('Database opened');
+
+// enable graceful stop
+process.once('SIGINT', () => {
+    db.close();
+    console.debug('Database closed');
+});
+process.once('SIGTERM', () => {
+    db.close();
+    console.debug('Database closed');
+});
