faa5ffc63f7f3eb3841517ded07c5ec8b24cf3b9
TheAihopGG <108916224+TheAihopGG@users.noreply.github.com>
2025-01-04 13:26:36 +0700

    fix: replaced loadSettings with settings. Now settings loads after module import

--- src/services/settings.ts
+++ src/services/settings.ts
@@ -1,28 +1,21 @@
 import { readFileSync } from 'fs';
-/**
-* Contains interfaces for data/appSettings.json. Use ISettings, when you loads settings file.
-*/
-export namespace SettingsInterfaces {
-    interface IPaths {
-        /** Contains path to database file*/
-        database: string
-    }
+import * as dotenv from 'dotenv';
 
-    export interface ISettings {
-        /** Contains paths to static files*/
-        paths: IPaths,
-        /** Contains listening ports*/
-        port: number
-    }
+// init environments parameters
+dotenv.config();
+
+interface IPaths {
+    /** Contains path to database file*/
+    database: string
 }
 
-/**
- * Returns object implements ISettings
- * 
- * @param path - path to settings file
-*/
-export function loadSettings(path: string = process.env.APP_SETTINGS_PATH): SettingsInterfaces.ISettings {
-    return JSON.parse(
-        readFileSync(path).toString()
-    );
+export interface ISettings {
+    /** Contains paths to static files*/
+    paths: IPaths,
+    /** Contains listening ports*/
+    port: number
 }
+
+export const settings: ISettings = JSON.parse(
+    readFileSync(process.env.APP_SETTINGS_PATH).toString()
+);
