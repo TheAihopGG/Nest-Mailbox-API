ae91ea002efe691593b5a44fc694348a90c1c95f
TheAihopGG <108916224+TheAihopGG@users.noreply.github.com>
2025-01-04 12:50:15 +0700

    feat: launch app and test request

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,9 @@
 import * as dotenv from 'dotenv';
-import { loadSettings } from './services/settings';
 import * as sqlite3 from 'sqlite3';
+import * as express from 'express';
+import { loadSettings } from './services/settings';
+import { createTables } from './services/database';
+import { request } from 'http';
 
 // init environments parameters
 dotenv.config();
@@ -12,9 +15,24 @@ const db = new sqlite3.Database(
     sqlite3.OPEN_CREATE | sqlite3.OPEN_READWRITE
 );
 console.debug('Database opened');
+// create app
+const app = express();
+
+// init database
+createTables(db);
+console.debug('Tables created');
+
+// test
+app.get('/', (request, response) => {
+    response.send('<h1>Hello</h1>');
+});
 
 // enable graceful stop
 process.once('beforeExit', () => {
     db.close();
     console.debug('Database closed');
 });
+
+// launch app
+console.debug(`Listening port ${settings.port}`);
+app.listen(settings.port);
