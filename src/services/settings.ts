ef864e1fbad7e46661f61b44263f55f32f488a96
TheAihopGG <108916224+TheAihopGG@users.noreply.github.com>
2025-01-04 10:45:38 +0700

    feat: loadSettings method

--- src/services/settings.ts
+++ src/services/settings.ts
@@ -1,3 +1,4 @@
+import { readFileSync } from 'fs';
 /**
 * Contains interfaces for data/appSettings.json. Use ISettings, when you loads settings file.
 */
@@ -16,4 +17,15 @@ export namespace SettingsInterfaces {
         paths: IPaths,
         ports: number[]
     }
+}
+
+/**
+ * Returns object implements ISettings
+ * 
+ * @param path - path to settings file
+*/
+export function loadSettings(path: string): SettingsInterfaces.ISettings {
+    return JSON.parse(
+        readFileSync(process.env.APP_SETTINGS_PATH).toString()
+    );
 }
